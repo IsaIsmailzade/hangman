f64db73f749eb1e8bf91cef52dbb41fd15fcf3d6
Isa <isaismzade@gmail.com>
2024-10-31 19:54:19 +0300

    Save 31.10.24

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Path path = Path.of("Words.txt");
-        System.out.println("[N]ew game or [E]xit");
+        System.out.println("[Н]овая игра или [В]ыйти");
 
         try (Scanner scanner = new Scanner(System.in);
              BufferedReader bufferedReader = Files.newBufferedReader(path)) {
             String start = scanner.nextLine();
             String word = bufferedReader.readLine();
 
-            if (start.equals("N") || start.equals("n")) {
-                for (int i = 0; i < word.length(); i++) {
-                    System.out.print("*");
+            if (start.equals("Н") || start.equals("н")) {
+                int remainingLetters = word.length();
+                String findWord = "";
+
+                while (remainingLetters > 0) {
+                    System.out.println("\nВведите букву: ");
+
+                    String letter = scanner.nextLine();
+                    for (int i = 0; i < word.length(); i++) {
+
+                        if (Character.toLowerCase(word.charAt(i)) == Character.toLowerCase(letter.charAt(0))) {
+                            findWord = String.valueOf(word.charAt(i));
+                            remainingLetters--;
+                        }
+                    }
+                    System.out.print(findWord);
+                    for (int i = 0; i < word.length() - 1; i++) {
+                        System.out.print("*");
+                    }
                 }
-                System.out.println("\nВведите букву: ");
-                String letter = scanner.nextLine();
             }
         }
     }
